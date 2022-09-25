75240f8d7bd241170708ca85ee1a4fb699e89eb6
Pedro Reis <pedronr0309@gmail.com>
2022-09-25 13:28:41 -0300

    build: starting project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(PORT);
+}
+bootstrap();
